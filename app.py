dd3a33f62527381d64035f1b6be4d9d87232ad8d
EduardoSLuz <edyven100@gmail.com>
2023-05-14 17:30:07 -0400

    feat: updating add.py

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/teste", methods=["POST"])
-def teste():
-   return jsonify({'message': 'otimo mano'})
+@app.route("/api", methods=["POST"])
+def api():
+   return jsonify({'message': 'otima API mano'})
 
-@app.route("/foda", methods=["GET"])
-def teste2():
-   return jsonify({'message': 'FOFA'})
+@app.route("/user", methods=["GET"])
+def user():
+   return jsonify({'message': 'USER FOFO'})
+
+@app.route("/news", methods=["PUT"])
+def news():
+   return jsonify({'message': 'NEWS GRANDE DEMAIS'})
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=5000, debug=True)
+   app.run(host='0.0.0.0', port=3000, debug=True)
