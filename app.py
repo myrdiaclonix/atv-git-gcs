a66a82b5a22002328dd75b7c4bc8d7713cad22b1
EduardoSLuz <edyven100@gmail.com>
2023-05-14 17:39:46 -0400

    feat: good lucky

--- app.py
+++ app.py
@@ -2,13 +2,20 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/api", methods=["POST"])
+def readFile(path):
+   f = open(path, "r")
+   archive = f.read()
+   f.close()
+   return archive
+
+@app.route("/api-teste", methods=["POST"])
 def api():
    return jsonify({'message': 'otima API mano'})
 
 @app.route("/user", methods=["GET"])
 def user():
-   return jsonify({'message': 'USER FOFO'})
+   file = readFile("arquivo.txt")
+   return jsonify({'message': 'USER FOFO', 'res': file})
 
 @app.route("/news", methods=["PUT"])
 def news():
