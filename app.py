d903407e2a20b2ad47a1c083a3d8a15f97ddc352
RSobrinho <rafarrsobrinho@hotmail.com>
2023-05-14 17:41:41 -0400

    feat: new functions

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from flask import Flask, request, jsonify
-
+import random
 app = Flask(__name__)
+
+def somaAleatoria():
+   return random.randint(1, 10)
+
+def randomDogBreed():
+   breeds = ["Labrador Retriever", "German Shepherd", "Golden Retriever", "French Bulldog", "Bulldog", "Poodle", "Beagle", "Dachshund", "Rottweiler", "Yorkshire Terrier"]
+   return random.choice(breeds)
+
 
 @app.route("/api", methods=["POST"])
 def api():
-   return jsonify({'message': 'otima API mano'})
+   return jsonify({'message': f'otima API mano que legal uhulll {randomDogBreed()}'})
 
 @app.route("/user", methods=["GET"])
 def user():
-   return jsonify({'message': 'USER FOFO'})
+   return jsonify({'message': f'USER FOFO {somaAleatoria()} '})
 
 @app.route("/news", methods=["PUT"])
 def news():
-   return jsonify({'message': 'NEWS GRANDE DEMAIS'})
+   return jsonify({'message': 'NEWS GRANDE DEMAIS, se está doido, festa semana que vem ein'})
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=3000, debug=True)
+   app.run(host='0.0.0.0', port=3333, debug=True)
